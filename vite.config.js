b6b4384d764b2259aea5b467a517c45cdfa26b25
gramtop961 <gramtop961@gmail.com>
2025-10-02 03:08:50 +0530

    Add vite.config.js from template

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    // Exclude packages that shouldn't be pre-bundled
+    exclude: [],
+    // Entries point for dependency pre-bundling
+    entries: ['./src/**/*.{js,jsx,ts,tsx}'],
+    // Hold the first optimizeDeps run until all dependencies are discovered
+    holdUntilCrawlEnd: true
+  },
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    strictPort: true,
+    hmr: false,
+    watch: false,
+    cors: {
+      origin: (origin, callback) => {
+        if (!origin || origin.includes('.modal.host')) {
+          callback(null, true);
+        } else {
+          callback(null, false);
+        }
+      }
+    }
+  }
+})
